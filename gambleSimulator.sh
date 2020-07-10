4787ad88cedb5ac12ec216865af1072ddf6ea695
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-10 15:25:46 +0530

    [agosh]REFACTOR:Changed constant variables

--- gambleSimulator.sh
+++ gambleSimulator.sh
@@ -3,13 +3,12 @@ STAKE_AMOUNT=100;
 BET_AMOUNT=1;
 PY_LOST=0;
 PY_WON=1;
+PERCENT_STAKE=$((($STAKE_AMOUNT*50)/100))
 
-percentStake=$((($STAKE_AMOUNT*50)/100))
-winLimit=$((percentStake+STAKE_AMOUNT))
-loseLimit=$((STAKE_AMOUNT-percentStake))
-dayCount=1;
+winLimit=$((PERCENT_STAKE+STAKE_AMOUNT))
+loseLimit=$((STAKE_AMOUNT-PERCENT_STAKE))
 
-echo "Day-$dayCount:Press 1 to place the bet for today"
+echo "Press 1 to place the bet "
  read userValue
   while  (($userValue == "1" )) && (( $STAKE_AMOUNT < $winLimit )) && (( $STAKE_AMOUNT > $loseLimit))
   do
@@ -18,11 +17,9 @@ echo "Day-$dayCount:Press 1 to place the bet for today"
    then
         echo "you lost $"$BET_AMOUNT
          STAKE_AMOUNT=$((STAKE_AMOUNT-1));
-          echo $lostToday
    else
         echo "you won $"$BET_AMOUNT
          STAKE_AMOUNT=$((STAKE_AMOUNT+1));
-          echo $winToday
    fi
  echo -e "Toatal Amount =$STAKE_AMOUNT\nTo Continue press 1"
  read userValue

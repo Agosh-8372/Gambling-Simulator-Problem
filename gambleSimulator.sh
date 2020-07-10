364b32bf963eed501c1b0481a52457153ce44b3a
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-10 21:07:15 +0530

    [agosh]ADD:Everyday win or loose amount for 20 days

--- gambleSimulator.sh
+++ gambleSimulator.sh
@@ -1,27 +1,54 @@
-#!/bin/bash -x
+INITIAL=100;
 STAKE_AMOUNT=100;
 BET_AMOUNT=1;
 PY_LOST=0;
-PY_WON=1;
 PERCENT_STAKE=$((($STAKE_AMOUNT*50)/100))
+WIN_LIMIT=$((PERCENT_STAKE+STAKE_AMOUNT))
+LOSE_LIMIT=$((STAKE_AMOUNT-PERCENT_STAKE))
 
-winLimit=$((PERCENT_STAKE+STAKE_AMOUNT))
-loseLimit=$((STAKE_AMOUNT-PERCENT_STAKE))
+declare -a arr_Win=()
+declare -a arr_lost=()
+day=1
+num_of_Days=20;
 
-echo "Press 1 to place the bet "
- read userValue
-  while  (($userValue == "1" )) && (( $STAKE_AMOUNT < $winLimit )) && (( $STAKE_AMOUNT > $loseLimit))
-  do
-       random=$((RANDOM%2))
-   if [[ $random -eq $PY_LOST ]]
-   then
-        echo "you lost $"$BET_AMOUNT
-         STAKE_AMOUNT=$((STAKE_AMOUNT-1));
-   else
-        echo "you won $"$BET_AMOUNT
-         STAKE_AMOUNT=$((STAKE_AMOUNT+1));
-   fi
- echo -e "Toatal Amount =$STAKE_AMOUNT\nTo Continue press 1"
- read userValue
- done
-echo "You pressed the wrong key Or Todays limit is reached "
+function day_WinLoose()
+{
+	dayWin=$1
+	dayLose=$2
+ 	dayCount=$3
+		if [ $dayWin -gt $dayLose ]
+		then
+			echo "you win today $"$((dayWin-dayLose))
+			arr_Win[(($dayCount-1))]=$((dayWin-dayLose))
+		else
+			echo "you lost today $"$((dayLose-dayWin))
+			arr_Lose[(($dayCount-1))]=$((dayLose-dayWin))
+		fi
+}
+
+
+for((i=$day;i<=$num_of_Days;i++))
+do
+	totalWin=0;
+   totalLost=0;
+	echo "Day $day-Press 1 to place the bet "
+ 	read userValue
+  		while  (($userValue == "1" )) && (( $STAKE_AMOUNT < $WIN_LIMIT )) && (( $STAKE_AMOUNT > $LOSE_LIMIT))
+  		do
+       	random=$((RANDOM%2))
+   			if [[ $random -eq $PY_LOST ]]
+   			then
+         		STAKE_AMOUNT=$((STAKE_AMOUNT-1));
+					((totalLost++))
+   			else
+         		STAKE_AMOUNT=$((STAKE_AMOUNT+1));
+					((totalWin++))
+   			fi
+ 		done
+  	day_WinLoose $totalWin $totalLost $day
+	((day++))
+	STAKE_AMOUNT=$((STAKE_AMOUNT+INITIAL))
+	PERCENT_STAKE=$((($STAKE_AMOUNT*50)/100))
+	WIN_LIMIT=$((PERCENT_STAKE+STAKE_AMOUNT))
+	LOSE_LIMIT=$((STAKE_AMOUNT-PERCENT_STAKE))
+done

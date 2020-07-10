2ae4cf19114fb422e69cdc8cc974b8b83babe2e7
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-10 08:37:13 +0530

    [agosh]Add:Check win or loose

--- gambleSimulator.sh
+++ gambleSimulator.sh
@@ -1,4 +1,19 @@
 #!/bin/bash -x
 STAKE_AMOUNT=100;
 BET_AMOUNT=1;
-
+PY_LOST=0;
+PY_WON=1;
+echo "Press 1 to place the bet"
+ read userValue
+  while [[ $userValue == "1" ]]
+  do
+       random=$((RANDOM%2))
+   if [[ $random -eq 0 ]]
+   then
+        echo "you lost $"$BET_AMOUNT
+   else
+        echo "you won $"$BET_AMOUNT
+   fi
+ echo "To Continue press 1"
+ read userValue
+ done

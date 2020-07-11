3faf2159180c22b8fe02937452446323ea4e5c5a
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-11 16:23:23 +0530

    [agosh]ADD:function for Lucky & unLucky day and Amount

--- gambleSimulator.sh
+++ gambleSimulator.sh
@@ -6,8 +6,8 @@ PERCENT_STAKE=$((($STAKE_AMOUNT*50)/100))
 WIN_LIMIT=$((PERCENT_STAKE+STAKE_AMOUNT))
 LOSE_LIMIT=$((STAKE_AMOUNT-PERCENT_STAKE))
 
-day=1
-num_of_Days=30;
+NUM_OF_DAYS=30;
+day=1;
 
 declare -a arr_Win
 declare -a arr_Lose
@@ -27,13 +27,39 @@ function day_WinLoose()
 		fi
 }
 
-for((i=$day;i<=$num_of_Days;i++))
+function Lucky()
+{
+	max=50;
+		for findMax in "${arr_Win[@]}" ; do
+    		((findMax > max)) && max=$findMax
+		done
+	value=$max
+		for findIndex in "${!arr_Win[@]}"; do
+   		if [[ "${arr_Win[$findIndex]}" = "${value}" ]]; then
+       		echo -e "\nLucky Day=${findIndex} and Amount Won=$max";
+   		fi
+		done
+}
+
+function unLucky()
+{
+	min=50;
+		for findMin in "${arr_Lose[@]}" ; do
+    		((findMin > min)) && min=$findMin
+		done
+	value=$min
+		for findMinIndex in "${!arr_Lose[@]}"; do
+   		if [[ "${arr_Lose[$findMinIndex]}" = "${value}" ]]; then
+       		echo -e "\nunLucky Day=${findMinIndex} and Amount Lost=$min";
+   		fi
+		done
+}
+
+for((counter=$day;counter<=$NUM_OF_DAYS;counter++))
 do
 	dayWinCount=0;
    dayLostCount=0;
-	echo "Day $day-Press 1 to place bet "
- 	read userValue
-  		while (( $userValue == "1" )) && (( $STAKE_AMOUNT < $WIN_LIMIT )) && (( $STAKE_AMOUNT > $LOSE_LIMIT ))
+  		while (( $STAKE_AMOUNT < $WIN_LIMIT )) && (( $STAKE_AMOUNT > $LOSE_LIMIT ))
   		do
        	random=$((RANDOM%2))
    			if [[ $random -eq $PY_LOST ]]
@@ -54,3 +80,5 @@ do
 done
  echo -e "Days on which player Won and Amount:\nDay=${!arr_Win[@]}\n${arr_Win[@]}"
  echo -e "Days on which player Lost and Amount:\nDay=${!arr_Lose[@]}\n${arr_Lose[@]}"
+ Lucky
+ unLucky
